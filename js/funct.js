a32fe8abf0e6101d8f3a5172e9bc6cdc52d6ff86
PaoloFabris98 <f98paolo@gmail.com>
2024-10-17 22:26:05 +0200

    feat: completed

--- js/funct.js
+++ js/funct.js
@@ -1,4 +1,4 @@
-export function isString(word) {
+function isString(word) {
   return typeof word === "string";
 }
 
@@ -16,5 +16,36 @@ export function palindroCheck(palindroIN) {
     }
   } else {
     console.log("Quanto digitato non può essere un numero");
+  }
+}
+
+const min = 1;
+const max = 5;
+
+function randomNum(min, max) {
+    const temp = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(temp);
+    return temp;
+}
+
+export function evenOddNumberProcessing(choice, number) {
+  const randomValue = randomNum(min, max);
+  console.log(randomValue);
+  const prova = Number(number) + Number(randomValue);
+  console.log(prova);
+  console.log(`Hai scelto: ${number}`);
+  console.log(`Hai scelto: ${choice}`);
+  if (choice === "pari") {
+    if (prova % 2 === 0) {
+      console.log(`Il numero ${prova} è pari. Hai vinto!`);
+    } else if (prova % 2 !== 0) {
+      console.log(`Il numero ${prova} è dispari. Hai perso!`);
+    }
+  } else {
+    if (prova % 2 === 0) {
+      console.log(`Il numero ${prova} è pari. Hai perso!`);
+    } else if (prova % 2 !== 0) {
+      console.log(`Il numero ${prova} è dispari. Hai vinto!`);
+    }
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-import { isString } from "./funct.js";
 import { palindroCheck } from "./funct.js";
+import { evenOddNumberProcessing } from "./funct.js";
 
 //inserimento parola palindroma
 const palindroIN = prompt("Digita una parola:");
+const evenOddchoice = prompt("Scegli se pari o dispari:").toLocaleLowerCase();
+const evenOddNumber = prompt("Scegli il tuo numero:");
 
 palindroCheck(palindroIN);
+evenOddNumberProcessing(evenOddchoice,evenOddNumber);
+
